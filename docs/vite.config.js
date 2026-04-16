ee19da0e69789b7eae539bb72bf3bb1fb4535ee1
ryan-houseman <houseman.r@northeastern.edu>
2026-04-16 17:23:07 -0400

    update to vite config

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/project-spring26-ryan-houseman/',
+  base: '/project-spring26-ryan-houseman/docs/',
   server: {
     proxy: {
       '/api': 'http://127.0.0.1:5000'
