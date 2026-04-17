ba102cca2cdcc6ab1a5e17bde5d4c6dd93f681e2
ryan-houseman <houseman.r@northeastern.edu>
2026-04-17 15:16:46 -0400

    updates to github pages deployment

--- reporting/client/vite.config.js
+++ reporting/client/vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default {
-  server: {
-    proxy: {
-      '/api': 'http://127.0.0.1:5000'
-    }
+export default defineConfig({
+  base: '/project-spring26-ryan-houseman/docs/',
+  build: {
+    outDir: 'docs'
   },
   plugins: [react()],
-}
+})
